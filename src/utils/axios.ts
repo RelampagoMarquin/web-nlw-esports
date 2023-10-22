a7f75d3026be80e8e35ab4cc545cb0096f50099e
Marcos Moreira <marcos_more@outlook.com>
2023-10-21 21:30:34 -0300

    teste

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiAxios =  axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL?? "http://back:3333",
     headers: {
       'Content-Type': 'application/json',
       'Accept': '*/*',
