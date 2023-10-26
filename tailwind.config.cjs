3326315d67523ae856677aa0a997a95e0e4222b7
Marcos Moreira <88684449+RelampagoMarquin@users.noreply.github.com>
2023-10-26 15:20:38 -0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        galaxy: "url('/Fundo.jpg')",
+        galaxy: "url('/Fundo.png')",
         'nlw-gradient': 'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)',
         'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       }
